95391eed25953b9e8dcba4c2b18d82efc8e22e7a
EmmanuelSanchez77 <al222310365@gmail.com>
2025-02-15 08:38:45 -0600

    Incluye carpetas vacías con .gitkeep

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/asdsd</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>
+        este es un texto
+      </p>
+      desarrollo en dev
       </header>
     </div>
   );
